3356988acdd66299f8c755e0ad19361d05c91f3f
nachet <nachet@localhost>
2025-10-02 12:12:21 +0200

    JUNIT

--- src/RepositorioPublicacionesTest.java
+++ src/RepositorioPublicacionesTest.java
@@ -41,7 +41,7 @@ public class RepositorioPublicacionesTest extends TestCase {
 		Publicacion buscar1 = rP.buscarPubliPorId("P1");
 		assertEquals("P1", buscar1.getId());
 		assertEquals("Publi1", buscar1.getTítulo());
-		
+			
 		Publicacion buscar2 = rP.buscarPubliPorId("P2");
 		assertEquals("P2", buscar2.getId());
 		assertEquals("Publi2", buscar2.getTítulo());
